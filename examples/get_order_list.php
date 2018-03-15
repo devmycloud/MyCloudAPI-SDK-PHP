8c22cd8d5e5bfb17bb89c88a704d05ef34b96090
Tim Endres <tim@bkbasic.com>
2018-03-15 04:06:52 -0400

    Updated to implement paging

--- examples/get_order_list.php
+++ examples/get_order_list.php
@@ -7,8 +7,18 @@ require 'printers.php';
 use MyCloud\Api\Core\MCError;
 use \MyCloud\Api\Model\Order;
 
+// ARGUMENTS:
+//   [1] List offset
+//   [2] Product count
+
+$offset = 0;
+$count = 10;
+
+if ( $argc > 1 ) { $offset = $argv[1]; }
+if ( $argc > 2 ) { $count = $argv[2]; }
+
 try {
-	$orders = Order::all( array(), null );
+	$orders = Order::all( array( 'offset' => $offset, 'count' => $count ) );
 
 	if ( $orders instanceof MCError ) {
 		print "ERROR retrieving order list:" . PHP_EOL;
--- examples/get_product_list.php
+++ examples/get_product_list.php
@@ -7,8 +7,18 @@ require 'printers.php';
 use MyCloud\Api\Core\MCError;
 use MyCloud\Api\Model\Product;
 
+// ARGUMENTS:
+//   [1] List offset
+//   [2] Product count
+
+$offset = 0;
+$count = 10;
+
+if ( $argc > 1 ) { $offset = $argv[1]; }
+if ( $argc > 2 ) { $count = $argv[2]; }
+
 try {
-	$products = Product::all();
+	$products = Product::all( array( 'offset' => $offset, 'count' => $count ) );
 
 	if ( $products instanceof MCError ) {
 		print "ERROR retrieving product list:" . PHP_EOL;
